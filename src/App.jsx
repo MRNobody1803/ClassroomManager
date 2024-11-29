091510d8f14e938c3a337e57f617b81d2d992ead
Abdes <abdessamadelhachami@gmail.com>
2024-11-29 21:07:31 +0100

    Home-LogIn page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import './App.css'
+import Home from './Components/Login/Home';
 import LoginForm from './Components/Login/LoginForm';
+import Footer from './Components/SharedComponents/Footer';
 import HeaderMain from './Components/SharedComponents/HeaderMain';
 
 
 function App() {
 
   return (
-    <div>
-      <div className='home'>
+    <div className='page'>
+      <div className='homeApp'>
         <HeaderMain/>
       </div>
-      <div className="form">
-        <LoginForm/>
+      <div className="crd">
+        <Home/>
       </div>
+      <div>
+        <Footer/>
+      </div>
+      
     </div>
     
   );
